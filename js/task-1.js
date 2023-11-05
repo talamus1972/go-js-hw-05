a9df0e37f9be022613f73399b68dee01035f9b11
Andrii <140251335+talamus1972@users.noreply.github.com>
2023-11-05 20:33:04 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,30 @@
+function isEnoughCapacity(products, containerSize) {
+  
+   let totalSalary = 0;
+    
+    const values = Object.values(products);
+  for (const key of values) {
+    totalSalary += key; 
+  }
+    if (totalSalary <= containerSize)
+      { 
+      return true;
+    }
+     return false;   
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
